8eb98372aac7493e33e9e0a06f5913a40103fe04
pipeeex <72838771+pipeeex@users.noreply.github.com>
2025-10-12 22:23:58 -0500

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ import sanitize from "sanitize-filename";
 const app = express();
 const PORT = 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 app.use(express.static("public"));
 
